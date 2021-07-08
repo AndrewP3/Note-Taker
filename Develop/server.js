533e5765512f6940ed6f28ca497165fb482b4e54
AndrewP3 <andrewpowell097@gmail.com>
2021-07-08 12:50:25 -0700

    Third commit

--- Develop/server.js
+++ Develop/server.js
@@ -7,3 +7,20 @@ const fs = require("fs");
 // For future routes
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(express.static("public"));
+
+// app.use routes will go here
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+// Make server listen
+app.listen(PORT, () => {
+  console.log("API server is now on port ${PORT}!");
+});
