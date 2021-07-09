8f76e963782ff2cbab21f4d931277a84dfa3bc55
AndrewP3 <andrewpowell097@gmail.com>
2021-07-08 18:42:56 -0700

    6th commit

--- Develop/routes/apiRoutes/notes.js
+++ Develop/routes/apiRoutes/notes.js
@@ -1,8 +1,23 @@
 const router = require("express").Router();
 const path = require("path");
+const {
+  createNewNote,
+  deleteNote,
+} = require("../../../../note-taker-main/lib/noteFunctions");
 
 router.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+router.post("/notes", function (req, res) {
+  req.body.id = notes.length.toString();
+  let note = createNewNote(req.body, notes);
+  res.json(note);
+});
+
+router.delete("/notes/:id", function (req, res) {
+  deleteNote(notes, req.params.id);
+  res.json(notes);
 });
 
 module.exports = router;
